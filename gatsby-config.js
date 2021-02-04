6b5ecb77cc11d3ccae664abc8361f3cfca044d23
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2021-02-04 17:35:52 +0500

    checking netlify identity on deployed site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  plugins: ["gatsby-plugin-typescript", `gatsby-plugin-material-ui`],
+  plugins: [
+    "gatsby-plugin-typescript",
+    `gatsby-plugin-material-ui`,
+    {
+      resolve: `gatsby-plugin-create-client-paths`,
+      options: { prefixes: [`/todo/*`] },
+    },
+  ],
 };
